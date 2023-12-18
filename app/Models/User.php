c9d730b39e5718efa4ea2c959682c3b13cc6cb1f
Roman <romanyoga4@gmail.com>
2023-12-18 22:47:33 +0700

    last commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -42,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function rentals()
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\BookController;
+use App\Http\Controllers\Api\V1\RentalController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
@@ -21,6 +23,11 @@ Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
     Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
     Route::post('/register', RegisterController::class);
+});
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/books', BookController::class);
+    Route::apiResource('/rentals', RentalController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
